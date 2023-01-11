f48f5b24e7948f9a6a62ef5ef89324ec90f646d4
madhurjyasarma <madhurjyasarma>
2023-01-11 15:53:54 +0530

    Added front-end for asset management system

---
+++ src/app/_models/user.ts
@@ -0,0 +1,23 @@
+﻿export class User {
+    // Defined by the incoming REST API
+    id: string;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    token: string;
+}
+
+/*
+
+USER_Type : 
+
+u_id
+firstName
+lastName
+age
+gender
+address
+phoneNumber
+
+*/
